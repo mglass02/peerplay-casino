0db5069ba21483a89eb4b67372c87c8dc663085b
micha <michael.andrew.glass@gmail.com>
2024-09-25 22:41:34 +0100

    updated urls again

--- frontend/scripts/regOrLog.js
+++ frontend/scripts/regOrLog.js
@@ -9,7 +9,7 @@ document.getElementById('signup-form').addEventListener('submit', async (e) => {
 
   try {
     // Make POST request to backend signup route
-    const response = await fetch('https://sprightly-dolphin-5975b2/auth/signup', {  // Assuming backend is running on sprightly-dolphin-5975b2
+    const response = await fetch('https://sprightly-dolphin-5975b2.netlify.app/auth/signup', {  // Assuming backend is running on sprightly-dolphin-5975b2
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ username, email, password })
@@ -37,7 +37,7 @@ document.getElementById('login-form').addEventListener('submit', async (e) => {
   const password = document.getElementById('password-login').value;
 
   try {
-      const response = await fetch('https://sprightly-dolphin-5975b2/auth/login', {
+      const response = await fetch('https://sprightly-dolphin-5975b2.netlify.app/auth/login', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({ email, password })
--- frontend/scripts/slots.js
+++ frontend/scripts/slots.js
@@ -19,7 +19,7 @@ async function fetchUserPot() {
     }
 
     try {
-        const response = await fetch('https://sprightly-dolphin-5975b2/auth/user/funds', {
+        const response = await fetch('https://sprightly-dolphin-5975b2.netlify.app/auth/user/funds', {
             method: 'GET',
             headers: {
                 'Authorization': `Bearer ${token}`,
@@ -66,7 +66,7 @@ async function handlePayment(amount) {
     const token = localStorage.getItem('token');
 
     try {
-        const response = await fetch('https://sprightly-dolphin-5975b2/auth/user/play-game', {
+        const response = await fetch('https://sprightly-dolphin-5975b2.netlify.app/auth/user/play-game', {
             method: 'POST',
             headers: {
                 'Authorization': `Bearer ${token}`,
@@ -94,7 +94,7 @@ async function transferWinnings(amount) {
     const token = localStorage.getItem('token');
 
     try {
-        const response = await fetch('https://sprightly-dolphin-5975b2/auth/user/win', {
+        const response = await fetch('https://sprightly-dolphin-5975b2.netlify.app/auth/user/win', {
             method: 'POST',
             headers: {
                 'Authorization': `Bearer ${token}`,
